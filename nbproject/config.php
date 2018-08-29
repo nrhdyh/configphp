d8270d12125c05eb3b647f3d924a9be3c967d9d2
nrhdyh <35320146+nrhdyh@users.noreply.github.com>
2018-08-29 15:55:33 +0800

    Add files via upload

---
+++ nbproject/config.php
@@ -0,0 +1,8 @@
+<?php
+$databaseHost = 'localhost';
+$databaseName = 'simpan';
+$databaseUsername = 'root';
+$databasePassword ='123456';
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
+?>
